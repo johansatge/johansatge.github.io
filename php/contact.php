f8f4ad5d43773e2495860ffd055ba01c305a0de2
Johan <contact@johansatge.fr>
2014-05-04 21:40:41 +0200

    v0.2: wip

--- php/contact.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once 'classes/Contact.php';
-$contact = new Contact();
-$json    = $contact->handleRequest();
-header('application/json');
-echo json_encode(!empty($json) ? $json : array('idle'));
-exit;
