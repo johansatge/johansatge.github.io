0e2a442c2a3354f2a95deaf8a48499d54d833b69
Johan Satgé <jsatge@tequilarapido.com>
2014-05-05 14:04:03 +0200

    text styles

--- assets/js/site.js
+++ assets/js/site.js
@@ -3,7 +3,7 @@ var Site = (function()
     'use strict';
     function Site()
     {
-    }
+    };
     Site.init = function()
     {
         $('.bg').animate({opacity: 1}, 1000);
@@ -11,9 +11,16 @@ var Site = (function()
         var delay = 0;
         for(var index in steps)
         {
-            $(steps[index]).delay(delay).animate({opacity: 1, left: 0}, 200);
+            $(steps[index]).delay(delay).animate({opacity: 1, left: 0}, 400);
             delay += 50;
         }
+        $(window).on('resize', $.proxy(this, 'onWindowResize')).trigger('resize');
+    };
+    Site.onWindowResize = function()
+    {
+        var $content = $('.content');
+        var $window = $(window);
+        $content.css({top: (($window.height() - $content.height()) / 2) + 'px', left: (($window.width() - $content.width()) / 2) + 'px'});
     };
     return Site;
 })();
