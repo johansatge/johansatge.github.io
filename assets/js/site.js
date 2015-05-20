2dbcc030bfdada288ac0b13e1a398f750873bb0b
Johan <contact@johansatge.fr>
2015-05-20 23:02:55 +0200

    WIP

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,26 +1,14 @@
-var Site = (function()
+(function(window)
 {
     'use strict';
-    function Site()
+
+    var site = {};
+
+    site.init = function()
     {
+
     };
-    Site.init = function()
-    {
-        $('.bg').animate({opacity: 1}, 1000);
-        var steps = $('.js-step').get();
-        var delay = 800;
-        for(var index in steps)
-        {
-            $(steps[index]).delay(delay).animate({opacity: 1, left: 0}, 400);
-            delay += 50;
-        }
-        $(window).on('resize', $.proxy(this, 'onWindowResize')).trigger('resize');
-    };
-    Site.onWindowResize = function()
-    {
-        var $content = $('.content');
-        var $window = $(window);
-        $content.css({top: (($window.height() - $content.height()) / 2) + 'px', left: (($window.width() - $content.width()) / 2) + 'px'});
-    };
-    return Site;
-})();
+
+    window.Site = site;
+
+})(window);
